f459cc23e38a26d8f6ea41f3a7f2bc25ed74d036
wangchangli <changliwang2009@gmail.com>
2017-12-05 02:02:08 +0800

    update

--- install-mac.sh
+++ install-mac.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-brew install openssl libuv cmake zlib
+# brew install openssl libuv cmake zlib
 git clone https://github.com/uWebSockets/uWebSockets 
 cd uWebSockets
 git checkout e94b6e1
@@ -7,7 +7,9 @@ patch CMakeLists.txt < ../cmakepatch.txt
 mkdir build
 export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
 cd build
-OPENSSL_VERSION=`openssl version -v | cut -d' ' -f2`
+#OPENSSL_VERSION=`openssl version -v | cut -d' ' -f2`
+OPENSSL_VERSION=1.0.2m
+
 cmake -DOPENSSL_ROOT_DIR=$(brew --cellar openssl)/$OPENSSL_VERSION -DOPENSSL_LIBRARIES=$(brew --cellar openssl)/$OPENSSL_VERSION/lib ..
 make 
 sudo make install
